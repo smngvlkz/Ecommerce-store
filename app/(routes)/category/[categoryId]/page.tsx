1793dedc1df15f12fc6cb3dced8156f79fa5a732
TheCodingPantsula <asmdigital14@gmail.com>
2024-01-19 12:31:07 +0200

    final

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -2,7 +2,7 @@ import getCategory from "@/actions/get-category";
 import getColors from "@/actions/get-colors";
 import getProducts from "@/actions/get-products";
 import getSizes from "@/actions/get-sizes";
-import Billboard from "@/components/billboard";
+import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
 import Filter from "./components/filter";
 import NoResults from "@/components/ui/no-results";
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,5 +1,6 @@
-import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
+
+import { cn } from "@/lib/utils";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
@@ -8,22 +9,27 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
     return (
       <button
+        type={type}
         className={cn(
           `
-                w-auto
-                rounded-full
-                bg-black
-                border-transparent
-                px-5
-                py-3
-                disabled:opacity-50
-                text-white
-                font-semibold
-                hover:opacity-75
-                transition
-                `,
+        w-auto 
+        rounded-full 
+        bg-black
+        border
+        border-transparent
+        px-5 
+        py-3 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50
+        text-white
+        font-semibold
+        hover:opacity-75
+        transition
+      `,
+          disabled && "opacity-75 cursor-not-allowed",
           className
         )}
+        disabled={disabled}
         ref={ref}
         {...props}
       >
